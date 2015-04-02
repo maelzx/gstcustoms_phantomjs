8711c7ada46ee00fc60b48f43734cf2990d4e69e
maelzx <maelzx@gmail.com>
2015-04-02 17:00:38 +0800

    manage to trigger form submit
    yeay!

--- gstcustom2.js
+++ gstcustom2.js
@@ -22,42 +22,53 @@ page.onNavigationRequested = function(url, type, willNavigate, main) {
 
 page.open("https://gst.customs.gov.my/tap/_/#1", function(status){
     page.evaluate(function(){
-        // click
+        
         var e = document.createEvent('MouseEvents');
         e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
         document.querySelector("a#cl_b-i").dispatchEvent(e);
     });
-    setTimeout(function(){
-     page.render('gstcustoms1.png');   
+	
+	setTimeout(function(){
 
-     page.evaluate(function(){
-        // click
-        var e = document.createEvent('MouseEvents');
-        e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        document.querySelector("input#d-8").dispatchEvent(e);
-        setTimeout(function(){
-           document.querySelector("input#d-9").dispatchEvent(e);   
+		page.evaluate(function(){
+			
+			var e = document.createEvent('MouseEvents');
+			e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			document.querySelector("input#d-8").dispatchEvent(e);
+		
+			setTimeout(function(){
+				
+				var e = document.createEvent('MouseEvents');
+				e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+				document.querySelector("input#d-9").dispatchEvent(e);   
 
-           setTimeout(function(){
-               document.getElementById("d-9").value = "MYDIN"; 
+				setTimeout(function(){
+					
+					document.getElementById("d-9").focus();
+					document.getElementById("d-9").value = "MYDIN"; 
 
-               setTimeout(function(){
-                   document.querySelector("input#d-7").dispatchEvent(e); 
-               }, 1000);
-               
-           }, 1000);
+					setTimeout(function(){
+					
+						//var e = document.createEvent('MouseEvents');
+						//e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+						//document.querySelector("input#d-7").dispatchEvent(e); 
+						document.getElementById("d-7").focus();
+					
+					
+					}, 1000);
+				   
+				}, 1000);
 
-       }, 1000);
-        
-    });
-
-
-     setTimeout(function(){
-         page.render('gstcustoms2.png');  
-         phantom.exit()   
-     }, 10000);
+			}, 1000);
+			
+		});
 
  }, 10000);
+ 
+	setTimeout(function(){
+			page.render('gstcustoms2.png');  
+			phantom.exit()   
+	}, 15000);
 
 
 
